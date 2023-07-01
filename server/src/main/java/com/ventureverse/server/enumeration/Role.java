754550ee5379311929f8391ccf6e36ae37ac4bb7
Samindu <2020cs025@stu.ucsc.cmb.ac.lk>
2023-07-02 00:51:58 +0530

    Update Role.java

--- server/src/main/java/com/ventureverse/server/enumeration/Role.java
+++ server/src/main/java/com/ventureverse/server/enumeration/Role.java
@@ -1,6 +1,9 @@
 package com.ventureverse.server.enumeration;
 
 public enum Role {
-    USER,
-    ADMIN
+    ADMIN,
+    CO_ADMIN,
+    ENTERPRISE_INVESTOR,
+    INDIVIDUAL_INVESTOR,
+    ENTREPRENEUR,
 }
