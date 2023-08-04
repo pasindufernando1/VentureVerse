2f9b126c08986f4409f8c82a63e2ff398de5478e
Pasindu Fernando <2020cs051@stu.ucsc.cmb.ac.lk>
2023-08-04 10:56:15 +0530

    Updated avatars and progress bars
    Updated avatars and progress bars

--- client/src/pages/entrepreneur/ViewListing.js
+++ client/src/pages/entrepreneur/ViewListing.js
@@ -7,32 +7,42 @@ import {
     Button,
   } from "@material-tailwind/react";
 
+  
+
 import CustomButton from "../webcomponent/CustomButton";
 import ViewListingFull from "./ViewListingFull";
 import { useEffect, useState } from "react";
 import useAxiosMethods from "../../hooks/useAxiosMethods";
+import { Progress} from "@material-tailwind/react";
+import { Avatar } from "@material-tailwind/react";
 
 
 function ViewListing() {
 
     // Get request to get the listing details
     const { get } = useAxiosMethods();
-    const [listing, setListing] = useState({});
+    var [listing, setListing] = useState({});
+
 
     useEffect(() => {
         get("/entrepreneur/getListing/1604", setListing);
-        console.log(listing)
+        console.log(listing);
+         
     }, [])
 
     const [subView, setSubView] = useState(false);
     const [fullView, setFullView] = useState(true);
+    const [open, setOpen] = useState(false);
+
+    const onCloseDialog = () => {
+        setOpen(false);
+    };
 
     const onClicksub = () => {
         setSubView(true);
         setFullView(false);
+        setOpen(true);
     }
-
-
 
     return (
         <div className="h-auto min-h-[100vh] p-[2rem] flex justify-center items-center">
@@ -50,18 +60,14 @@ function ViewListing() {
                 </CardHeader>
                 <CardBody>
                     <Typography variant="h5" className="mb-2 text-main-purple">
-                        Title of the listing
+                        {listing.title}
                     </Typography>
                     <Typography>
-                        The place is close to Barceloneta Beach and bus stop just 2 min by
-                        walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                        night life in Barcelona.The place is close to Barceloneta Beach and bus stop just 2 min by
-                        walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                        night life in Barcelona.
+                        {listing.description}
                     </Typography>
                     {/* Business name */}
                     <Typography variant="h6" color="blue-gray" className="mb-2 mt-2">
-                        Business Name : ABC company
+                        Business Name : {listing.title}
                     </Typography>
                     <Typography variant="h5" className="mb-2 text-light-purple mt-4">
                             Expectations & Returns
@@ -93,6 +99,62 @@ function ViewListing() {
                             </Typography>
                         </div>
                     </div>
+                    <Typography variant="h5" className="mb-2 text-light-purple mt-4">
+                            Status
+                    </Typography>
+                    <div className="w-full">
+                        <div className="mb-2 flex items-center justify-between gap-4">
+                            <Typography color="blue" variant="h6" className="text-main-gray">
+                            Completed
+                            </Typography>
+                            <Typography color="blue" variant="h6" className="text-main-purple">
+                            50%
+                            </Typography>
+                        </div>
+                        <Progress value={50} color="purple"/>
+                    </div>
+                    <Typography variant="h6" className="mb-2 text-main-gray mt-4">
+                            Interested
+                    </Typography>
+                    {/* Div to show the images of interested parties images*/}
+                    <div className="flex items-center -space-x-4 mt-2">
+                        <Avatar
+                            variant="circular"
+                            alt="user 1"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 2"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 3"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 4"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            title="Mr. Nimal Fernando"
+                            src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 5"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                    </div>
+
                     {/*Section to show more details*/}
 
                 </CardBody>
--- client/src/pages/entrepreneur/ViewListingFull.js
+++ client/src/pages/entrepreneur/ViewListingFull.js
@@ -9,6 +9,8 @@ import {
 
 import CustomButton from "../webcomponent/CustomButton";
 import { Carousel, IconButton } from "@material-tailwind/react";
+import { Avatar } from "@material-tailwind/react";
+import { Progress} from "@material-tailwind/react";
 
 function ViewListingFull() {
 
@@ -67,6 +69,61 @@ function ViewListingFull() {
                                 $10
                             </Typography>
                         </div>
+                    </div>
+                    <Typography variant="h5" className="mb-2 text-light-purple mt-4">
+                            Status
+                    </Typography>
+                    <div className="w-full">
+                        <div className="mb-2 flex items-center justify-between gap-4">
+                            <Typography color="blue" variant="h6" className="text-main-gray">
+                            Completed
+                            </Typography>
+                            <Typography color="blue" variant="h6" className="text-main-purple">
+                            50%
+                            </Typography>
+                        </div>
+                        <Progress value={50} color="purple"/>
+                    </div>
+                    <Typography variant="h6" className="mb-2 text-main-gray mt-4">
+                            Interested
+                    </Typography>
+                    {/* Div to show the images of interested parties images*/}
+                    <div className="flex items-center -space-x-4 mt-2">
+                        <Avatar
+                            variant="circular"
+                            alt="user 1"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 2"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1061&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 3"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 4"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            title="Mr. Nimal Fernando"
+                            src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80"
+                        />
+                        <Avatar
+                            variant="circular"
+                            alt="user 5"
+                            className="border-2 border-white hover:z-10 focus:z-10"
+                            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80"
+                            title="Mr. Nimal Fernando"
+                        />
                     </div>
                     <Typography variant="h5" className="mb-2 text-light-purple mt-4">
                             Business / Product images
